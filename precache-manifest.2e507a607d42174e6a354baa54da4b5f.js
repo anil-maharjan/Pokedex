dae5aeb777565bd35909a67332523c00be7b637a
Anil <maharjananil68@gmail.com>
2020-06-24 08:12:08 +0545

    Updates

---
+++ precache-manifest.2e507a607d42174e6a354baa54da4b5f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8937fb8177e6479e46f73291429dfa4",
+    "url": "/pokedex2/index.html"
+  },
+  {
+    "revision": "ee4eaffee4eb295b5d63",
+    "url": "/pokedex2/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "0ac62bf449d60fbc20d3",
+    "url": "/pokedex2/static/css/main.6c863ae0.chunk.css"
+  },
+  {
+    "revision": "ee4eaffee4eb295b5d63",
+    "url": "/pokedex2/static/js/2.0e9f1f07.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokedex2/static/js/2.0e9f1f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ac62bf449d60fbc20d3",
+    "url": "/pokedex2/static/js/main.b02b38f2.chunk.js"
+  },
+  {
+    "revision": "5354285db63e0e327ef1",
+    "url": "/pokedex2/static/js/runtime-main.1761004f.js"
+  },
+  {
+    "revision": "f31c59d26c207703c200e5ee266dce55",
+    "url": "/pokedex2/static/media/pokdex.f31c59d2.png"
+  },
+  {
+    "revision": "31d2916eaea9a5447905b3f1c6a2209d",
+    "url": "/pokedex2/static/media/pokeball.31d2916e.png"
+  },
+  {
+    "revision": "50e5a11b866f739cf5904f1fc0f80535",
+    "url": "/pokedex2/static/media/pokeball2.50e5a11b.png"
+  }
+]);
